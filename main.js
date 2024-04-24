f436c4582a6303cca26ea950c55e21a6c439f746
artemgorbulya <gorbulya@gmail.com>
2024-04-24 15:21:10 +0300

    homework_2

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
-const logger = require("./utils/logger")("main");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const logger = require("./utils/logger")("main module");
 
 logger.info("the script is running!");
 logger.warn("warn message!");
--- utils/logger.js
+++ utils/logger.js
@@ -1,9 +1,31 @@
+const { colorsEnabled, logLevel } = require("config");
+const { green, yellow, red } = require("colors/safe");
+
 function logger(moduleName) {
-  return {
-    info: (...args) => console.log(bgBlue(`${moduleName}:`), ...args),
-    warn: (...args) => console.error(bgYellow(`${moduleName}:`), ...args),
-    error: (...args) => console.error(bgRed(`${moduleName}:`), ...args),
-  };
+  const shouldLogInfo = logLevel !== "warn" && logLevel !== "error";
+  const shouldLogWarn = logLevel !== "error";
+
+  const info = shouldLogInfo
+    ? (...args) =>
+        console.log(
+          colorsEnabled ? green(`${moduleName}:`) : `${moduleName}:`,
+          ...args
+        )
+    : () => {};
+  const warn = shouldLogWarn
+    ? (...args) =>
+        console.error(
+          colorsEnabled ? yellow(`${moduleName}:`) : `${moduleName}:`,
+          ...args
+        )
+    : () => {};
+  const error = (...args) =>
+    console.error(
+      colorsEnabled ? red(`${moduleName}:`) : `${moduleName}:`,
+      ...args
+    );
+
+  return { info, warn, error };
 }
 
 module.exports = logger;
